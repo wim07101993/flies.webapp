d7cb4d4b2c2723146fd15b3bfc6522be0333c788
wim van laer <van.laer.wim@gmail.com>
2019-06-04 19:40:50 +0200

    Created read and write file methods for service

--- participants/service.go
+++ participants/service.go
@@ -1,5 +1,11 @@
 package participants
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
 type Service struct {
 	filePath string
 }
@@ -8,4 +14,29 @@ func NewService(filePath string) Service {
 	return Service{
 		filePath: filePath,
 	}
+}
+
+func (pc *Service) readFile() ([]Participant, error) {
+	participants := []Participant{}
+	if _, err := os.Stat(pc.filePath); os.IsNotExist(err) {
+		return participants, pc.writeFile(participants)
+	}
+
+	bParticipants, err := ioutil.ReadFile(pc.filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bParticipants, &participants)
+	return participants, err
+}
+
+func (pc *Service) writeFile(participants []Participant) error {
+	jParticipants, err := json.Marshal(participants)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(pc.filePath, jParticipants, 664)
+	return err
 }
