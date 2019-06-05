dcf026fc921436d4113976842c01059ef6ad6b9b
wim van laer <van.laer.wim@gmail.com>
2019-06-05 10:51:28 +0200

    read settings from file

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/wim07101993/fly_swatting_contest/participants"
 )
+
+func readSettings(filePath string) Settings {
+	bsettings, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	var settings Settings
+	err = json.Unmarshal(bsettings, &settings)
+	if err != nil {
+		panic(err)
+	}
+
+	return settings
+}
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
 func main() {
+	var s Settings
+	if len(os.Args) > 1 {
+		s = readSettings(os.Args[1])
+	} else {
+		s = readSettings("settings.json")
+	}
 
-	ps := participants.NewService("participants.json")
+	ps := participants.NewService(s.ParticiPantsFilePath)
 	pc := participants.NewController(ps)
 
 	router := httprouter.New()
