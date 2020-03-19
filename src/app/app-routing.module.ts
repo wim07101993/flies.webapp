f11ac63a621024813b58ebc4523e7ab119b818dd
wim van laer <van.laer.wim@gmail.com>
2020-03-19 16:43:07 +0100

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ParticipantsComponent } from "./participants/participants.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ":year", component: ParticipantsComponent },
+  { path: "**", redirectTo: new Date().getFullYear().toString() }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/participants/participants.service.ts
+++ src/app/participants/participants.service.ts
@@ -1,40 +1,45 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { ParticipantDTO } from './participant';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { ParticipantDTO } from "./participant";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ParticipantsService {
+  private apiUrl: string = "http://10.101.90.59:5000/api";
 
-  private apiUrl: string = 'http://10.101.90.59:5000/api'
-  
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   createParticipant(participant: ParticipantDTO) {
     return this.http.post(`${this.apiUrl}/participants`, participant);
   }
 
-  getParticipants() : Observable<ParticipantDTO[]> {
+  getParticipants(): Observable<ParticipantDTO[]> {
     return this.http.get<ParticipantDTO[]>(`${this.apiUrl}/participants`);
   }
 
-  getParticipant(id: number) : Observable<ParticipantDTO> {
-    return this.http.get<ParticipantDTO>(`${this.apiUrl}/participants/${id}`)
+  getParticipant(id: number): Observable<ParticipantDTO> {
+    return this.http.get<ParticipantDTO>(`${this.apiUrl}/participants/${id}`);
   }
 
-  updateName(id: number, newName: string) : Observable<ParticipantDTO> {
-    return this.http.put<ParticipantDTO>(`${this.apiUrl}/participants/${id}/name/?name=${newName}`, "");
+  updateName(id: number, newName: string): Observable<ParticipantDTO> {
+    return this.http.put<ParticipantDTO>(
+      `${this.apiUrl}/participants/${id}/name/?name=${newName}`,
+      ""
+    );
   }
 
-  updateScore(id: number, score: number) : Observable<ParticipantDTO> {
-    return this.http.put<ParticipantDTO>(`${this.apiUrl}/participants/${id}/score/?score=${score}`, "");
+  updateScore(id: number, score: number): Observable<ParticipantDTO> {
+    return this.http.put<ParticipantDTO>(
+      `${this.apiUrl}/participants/${id}/score/?score=${score}`,
+      ""
+    );
   }
 
   deleteParticipant(id: number) {
-    return this.http.delete<ParticipantDTO>(`${this.apiUrl}/participants/${id}`)
+    return this.http.delete<ParticipantDTO>(
+      `${this.apiUrl}/participants/${id}`
+    );
   }
 }
