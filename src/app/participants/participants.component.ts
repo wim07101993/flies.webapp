af96ddafd99576b54235e186328b283a4ae5e78f
wim van laer <van.laer.wim@gmail.com>
2020-03-19 16:48:41 +0100

    added year from route to component

--- src/app/participants/participants.component.ts
+++ src/app/participants/participants.component.ts
@@ -1,34 +1,43 @@
-import { Component, OnInit, ViewChild, Inject } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material';
-import { ParticipantDTO } from './participant';
-import { ParticipantsService } from './participants.service';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit, ViewChild, Inject } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatSort } from "@angular/material";
+import { ParticipantDTO } from "./participant";
+import { ParticipantsService } from "./participants.service";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-participants',
-  templateUrl: './participants.component.html',
-  styleUrls: ['./participants.component.scss']
+  selector: "app-participants",
+  templateUrl: "./participants.component.html",
+  styleUrls: ["./participants.component.scss"]
 })
 export class ParticipantsComponent implements OnInit {
-
   displayedColumns: string[] = [
     //'id',
-    'name', 
-    'score',
-    'buttons'
+    "name",
+    "score",
+    "buttons"
   ];
   dataSource = new MatTableDataSource([]);
+  private year: number;
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(
-    private service: ParticipantsService, 
-    public dialog: MatDialog
-  ) { }
+    private service: ParticipantsService,
+    public dialog: MatDialog,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.refresh()
+    this.route.params.subscribe(params => {
+      this.year = +params["year"]; // '+' converts the string to a number
+      this.refresh();
+    });
   }
 
   applyFilter(filterValue: string) {
@@ -37,68 +46,62 @@ export class ParticipantsComponent implements OnInit {
 
   newParticipant() {
     const dialogRef = this.dialog.open(NewParticipantDialog, {
-      width: '250px',
-      data: { name: '', score: 0}
+      width: "250px",
+      data: { name: "", score: 0 }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if (result == null || result == ""){
+      if (result == null || result == "") {
         return;
       }
 
       this.service
-        .createParticipant({id: 0, name: result, score: 0})
+        .createParticipant({ id: 0, name: result, score: 0 })
         .subscribe(p => {
           this.refresh();
         });
-    })
+    });
   }
 
   getTotalSquashedFlies() {
-    return this.dataSource
-      .data
+    return this.dataSource.data
       .map(t => t.score)
       .reduce((acc, value) => acc + value, 0);
   }
 
   increaseParticipantScore(participant: ParticipantDTO) {
-    this.updateParticipantScore(participant, participant.score+1)
+    this.updateParticipantScore(participant, participant.score + 1);
   }
   decreaseParticipantScore(participant: ParticipantDTO) {
-    this.updateParticipantScore(participant, participant.score-1)
+    this.updateParticipantScore(participant, participant.score - 1);
   }
 
   updateParticipantScore(participant: ParticipantDTO, score: number) {
-    this.service
-      .updateScore(participant.id, score)
-      .subscribe((p) => {
-        participant.score = p.score
-        participant.name = p.name
-      });
+    this.service.updateScore(participant.id, score).subscribe(p => {
+      participant.score = p.score;
+      participant.name = p.name;
+    });
   }
 
   refresh() {
-    this.service
-      .getParticipants()
-      .subscribe((ps) => {
-        this.dataSource = new MatTableDataSource(ps);
-        this.dataSource.sort = this.sort;
-      });
+    this.service.getParticipants().subscribe(ps => {
+      this.dataSource = new MatTableDataSource(ps);
+      this.dataSource.sort = this.sort;
+    });
   }
 }
 
-
 @Component({
-  selector: 'app-new-participant-dialog',
-  templateUrl: './new-participant.dialog.html'
+  selector: "app-new-participant-dialog",
+  templateUrl: "./new-participant.dialog.html"
 })
 export class NewParticipantDialog {
-
   constructor(
     public dialogRef: MatDialogRef<NewParticipantDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: ParticipantDTO) { }
+    @Inject(MAT_DIALOG_DATA) public data: ParticipantDTO
+  ) {}
 
   onCancelClick(): void {
-    this.dialogRef.close(); 
+    this.dialogRef.close();
   }
 }
